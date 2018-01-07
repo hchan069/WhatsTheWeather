e241add69bb09c48c39480b1dc421c98400aacf6
spagabtti <spagabtti@gmail.com>
2018-01-07 15:25:51 -0800

    edit

---
+++ app/src/main/java/com/spaga/whatstheweather/MainActivity.java
@@ -0,0 +1,90 @@
+package com.spaga.whatstheweather;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText cityName;
+    Button findWeather;
+
+    public void findWeather(View view) {
+        Log.i("cityName", cityName.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityName = findViewById(R.id.cityName);
+        findWeather = findViewById(R.id.findWeather);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=" +
+                "b6907d289e10d714a6e88b30761fae22");
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            try {
+                url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String weatherInfo = jsonObject.getString("weather");
+
+                JSONArray array = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject jsonPart = array.getJSONObject(i);
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
